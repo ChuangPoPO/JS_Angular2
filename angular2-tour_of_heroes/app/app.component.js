f6483095915f09516d28c5a095eec06826c1fdab
ChuangPoPO <s842563@gmail.com>
2016-11-21 21:50:29 +0800

    create an array of ten heroes

--- angular2-tour_of_heroes/app/app.component.js
+++ angular2-tour_of_heroes/app/app.component.js
@@ -12,11 +12,7 @@ var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        //hero = 'Windstorm';
-        this.hero = {
-            id: 1,
-            name: 'Gaduo'
-        };
+        this.heroes = HEROES;
     }
     AppComponent = __decorate([
         core_1.Component({
@@ -35,4 +31,17 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
+//The HEROES array is of type Hero
+var HEROES = [
+    { id: 11, name: 'Mr. Chien' },
+    { id: 12, name: 'Mrs. Super' },
+    { id: 13, name: 'Dr. CV' },
+    { id: 14, name: 'Mrs. PoPO' },
+    { id: 15, name: 'Mr. Gaduo' },
+    { id: 16, name: 'Mr. GG' },
+    { id: 17, name: 'Mrs. vivian' },
+    { id: 18, name: 'Mr. Kevin' },
+    { id: 19, name: 'Mom' },
+    { id: 20, name: 'papa' }
+];
 //# sourceMappingURL=app.component.js.map
--- angular2-tour_of_heroes/app/app.component.ts
+++ angular2-tour_of_heroes/app/app.component.ts
@@ -20,18 +20,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Tour of Heroes';
+  heroes = HEROES;
   //hero = 'Windstorm';
-  hero: Hero = {
-     id: 1,
-     name: 'Gaduo'
-   };
+  // hero: Hero = {
+  //    id: 1,
+  //    name: 'Gaduo'
+  //  };
   
  }
 
 // hero property
  export class Hero {
-
    id: number;
    name: string;
-
  }
+
+//The HEROES array is of type Hero
+ const HEROES: Hero[] = [
+   {id:11 , name: 'Mr. Chien'},
+   {id:12 , name: 'Mrs. Super'},
+   {id:13 , name: 'Dr. CV'},
+   {id:14 , name: 'Mrs. PoPO'},
+   {id:15 , name: 'Mr. Gaduo'},
+   {id:16 , name: 'Mr. GG'},
+   {id:17 , name: 'Mrs. vivian'},
+   {id:18 , name: 'Mr. Kevin'},
+   {id:19 , name: 'Mom'},
+   {id:20 , name: 'papa'}
+ ];
