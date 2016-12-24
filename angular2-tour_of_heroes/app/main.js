1a257e20c472aa97267f5e22742a12594fd1e2ac
ChuangPoPO <s842563@gmail.com>
2016-12-24 22:48:29 +0800

    完成 issue#4，詳細訊息於gitHub上

--- angular2-tour_of_heroes/app/main.js
+++ angular2-tour_of_heroes/app/main.js
@@ -2,5 +2,7 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
 var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
+platform.bootstrapModule(app_module_1.AppModule)
+    .then(function (msg) { return console.log("Bootstrap success: " + msg); })
+    .catch(function (error) { return console.log(error); });
 //# sourceMappingURL=main.js.map
