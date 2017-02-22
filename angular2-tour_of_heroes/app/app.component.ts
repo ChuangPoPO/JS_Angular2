62cde1a6b468e7a5528e9055b0b18b7cd2594f7c
ChuangPoPO <s842563@gmail.com>
2017-02-22 19:23:59 +0800

    ADD NAVIGATION TO THE TEMPLATE

--- angular2-tour_of_heroes/app/app.component.ts
+++ angular2-tour_of_heroes/app/app.component.ts
@@ -5,10 +5,13 @@ import {Component} from '@angular/core';
 	selector: 'my-app',
 	template: `
 		<h1>{{title}}</h1>
-
-		<a routerLink = "/heroes">Heroes</a>
-		<router-outlet></router-outlet>
 		
+		<nav>
+			<a routerLink = "/dashboard">Dashboard</a>
+			<a routerLink = "/heroes">Heroes</a>
+		</nav>
+		
+		<router-outlet></router-outlet>
 
 		<!-- <my-heroes></my-heroes> -->
 	`
