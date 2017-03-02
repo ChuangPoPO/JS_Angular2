f81c6c01854f442dc1a30ed6dca62fe2da191d32
ChuangPoPO <s842563@gmail.com>
2017-03-02 19:05:51 +0800

    實作DashboardComponent，利用HeroService讀取並顯示前五位英雄

--- angular2-tour_of_heroes/app/dashboard.component.ts
+++ angular2-tour_of_heroes/app/dashboard.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+import { HeroService} from './hero.service';
+
+
 @Component({
 	moduleId: module.id,
 	selector: 'my-dashboard',
 	templateUrl: `./dashboard.component.html`,
 })
 
-export class DashboardComponent{}
+
+//implement the DashboardComponent
+export class DashboardComponent{
+
+	heroes: Hero[] = [];
+
+	constructor ( private heroService: HeroService) {}
+
+	ngOnInit(): void{
+		this.heroService.getHeroes()
+			.then ( heroes => this.heroes = heroes.slice(0,5) );
+	}
+}
