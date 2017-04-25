4c7fe694d354c4f3e5bc451db2112938ac07d70d
ChuangPoPO <s842563@gmail.com>
2017-04-25 19:34:19 +0800

    Update hero.service.ts But npm cannot start

--- angular2-tour_of_heroes/app/hero.service.ts
+++ angular2-tour_of_heroes/app/hero.service.ts
@@ -1,28 +1,36 @@
 import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import {Hero} from './hero'; //取得Hero型態
-import {HEROES} from './mock-heroes'; //匯入HEROES陣列中的資料
+
 
 @Injectable()
 
 export class HeroService {
 
+	private heroesUrl = 'api/heroes'; //URL to web api
+
+	constructor(private http:Http){}
+
 	getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+	.toPromise()
+	.then(response => response.json().data as Hero[])
+	.catch(this.handleError);
     }
 
+	private handleError(error:any):Promise<any>{
+		console.error('An error occurred', error); //for demo purouses only
+		return Promise.reject(error.message || error);
+	}
+
+	//Get hero by id
 	getHero(id:number): Promise <Hero> {
-		return this.getHeroes()
-		.then(heroes => heroes.find(hero => hero.id === id));
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.get(url)
+		.toPromise()
+		.then(response => response.json().data as Hero)
+		.catch(this.handleError);
 	} //stub
-
-
-	//讓資料取得時間變長
-	// getHeroesSlowly(): Promise<Hero[]> {
-  	// 	return new Promise(
-	//   	resolve => {
-	//     	// Simulate server latency with 2 second delay
-	//     	setTimeout(() => resolve(this.getHeroes()), 2000);
-  	// 	});
-  	// }
 }
