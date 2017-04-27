9aa00c8eccb0ba2b944907f79bb70437b378704e
ChuangPoPO <s842563@gmail.com>
2017-04-27 20:02:47 +0800

    更改了 package.json ，將 angular-in-memory-web-api 改 0.0.3 後執行 npm install，還是失敗

--- angular2-tour_of_heroes/app/app.module.ts
+++ angular2-tour_of_heroes/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
-// import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-// import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -23,7 +23,7 @@ import { RouterModule } from '@angular/router';
     FormsModule, //讓表單輸入支援雙向資料綁定
 		AppRoutingModule,
     HttpModule
-    //InMemoryWebApiModule.forRoot(InMemoryDataService)
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
 
   //For Angular recognizes the <my-heroes> tags
